fda87c010bb96a231833ac28e859ad0169877f4c
psingh1610 <33509378+psingh1610@users.noreply.github.com>
2020-08-30 19:24:34 -0500

    Update etl.py

--- etl.py
+++ etl.py
@@ -8,7 +8,7 @@ from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dat
 
 
 config = configparser.ConfigParser()
-#config.read('dl.cfg')
+config.read('dl.cfg')
 
 os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
@@ -142,7 +142,7 @@ def main():
     spark = create_spark_session()
     #input_data='data/'
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    
     #print(input_data)
     process_song_data(spark, input_data, output_data)    
     #process_log_data(spark, input_data, output_data)
